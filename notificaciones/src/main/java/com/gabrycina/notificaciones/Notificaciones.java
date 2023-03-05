99292fd8a2c4bc7e8941d0ee803c42e6c27ecd96
gabrycina <c.gabriele.info@gmail.com>
2023-03-05 13:29:07 +0100

    nuevos proyectos de microservicios

---
+++ notificaciones/src/main/java/com/gabrycina/notificaciones/Notificaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gabrycina.notificaciones;
+
+/**
+ *
+ * @author gabrycina
+ */
+public class Notificaciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
